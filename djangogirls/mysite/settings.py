9a80ab563e79e0a88ea05efba3dca00bcdeb2db2
Lee <leerwhiteno1@gmail.com>
2019-09-01 11:26:16 +0800

    add local to allowed hosts

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6owc=lxqzhqxydfy2x&#suoulb$6%qu+hnq0+2(##3w5ckhl*@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['leesdjangoblog.herokuapp.com']
+ALLOWED_HOSTS = ['leesdjangoblog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
