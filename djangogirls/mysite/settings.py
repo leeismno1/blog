2edf6c1be034c90883b9ba27db008fc0879201bb
Lee <leerwhiteno1@gmail.com>
2019-09-01 11:12:34 +0800

    Update Django for whitenoise static

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,5 +119,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
---
+++ python_intro.py
@@ -0,0 +1,15 @@
+def greeting_name(name):
+    print('Hi, ' + name.upper() + '!')
+
+
+greeting_name('Lee')
+
+girls = ["Rachel", "Monica", "Phoebe", "Ola"]
+
+for name in girls:
+    print("hi,", name)
+
+#Upto Template Extendgin on blog tutorial
+
+# activate virtual environment (myvenv is the name of the environment)= source myvenv/bin/activate
+# end virtual environment session = deactivate
